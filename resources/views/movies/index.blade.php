b5311c3fbae288ff5466b37f1fda8e4771249fe4
Matthieu Mota <matthieumota@gmail.com>
2023-07-17 18:51:37 +0200

    Fix flex

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="row row-cols-2 row-cols-md-4 row-cols-lg-5">
         @foreach ($movies as $movie)
-            <div class="col">
+            <div class="col d-flex flex-column">
                 <img class="img-fluid list-movie-image" src="{{ $movie->cover }}" alt="{{ $movie->title }}">
-                <div class="d-flex flex-column justify-content-between list-movie-content">
+                <div class="d-flex flex-column justify-content-between flex-grow-1">
                     <h3 class="list-movie-title">
                         <a href="/film/{{ $movie->id }}">
                             {{ $movie->title }}
