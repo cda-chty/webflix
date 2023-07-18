dce9df89939370f8ebb225cd67e421504c204fed
Matthieu Mota <matthieumota@gmail.com>
2023-07-18 15:17:41 +0200

    Account view

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
     public function index()
     {
-        return 'Bonjour '.Auth::user()->name;
+        return view('auth.account');
     }
 }
