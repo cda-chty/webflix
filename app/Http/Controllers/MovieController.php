12ee4a71c8341735014d963cb29df5f034317ddb
Matthieu Mota <matthieumota@gmail.com>
2023-07-19 11:17:28 +0200

    Upload when update

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
 
 class MovieController extends Controller
 {
@@ -58,6 +60,8 @@ class MovieController extends Controller
 
     public function edit(Movie $movie)
     {
+        Gate::allowIf($movie->user_id === Auth::user()->id);
+
         return view('movies.edit', [
             'categories' => Category::all(),
             'movie' => $movie,
@@ -66,6 +70,8 @@ class MovieController extends Controller
 
     public function update(Request $request, Movie $movie)
     {
+        Gate::allowIf($movie->user_id === $request->user()->id);
+
         $validated = $request->validate([
             // Pour le unique, on vérifie dans la table movies sauf pour le film modifié
             'title' => 'required|unique:movies,title,'.$movie->id.'|min:2',
@@ -77,6 +83,12 @@ class MovieController extends Controller
             'released_at' => 'nullable|date',
             'category' => 'required|exists:categories,id',
         ]);
+
+        // Remplacer l'image s'il y en a une
+        if ($request->hasFile('cover')) {
+            Storage::delete(str($movie->cover)->remove('/storage/'));
+            $validated['cover'] = '/storage/'.$request->file('cover')->store('movies');
+        }
 
         $validated['category_id'] = $validated['category']; // Fix column name
         unset($validated['category']);
@@ -91,6 +103,7 @@ class MovieController extends Controller
         $movie = Movie::findOrFail($id);
         Gate::allowIf($movie->user_id === $request->user()->id);
 
+        Storage::delete(str($movie->cover)->remove('/storage/')); // Supprimer l'image
         Movie::destroy($id);
 
         return redirect('/films')->with('message', 'Le film a été supprimé.');
