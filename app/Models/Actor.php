bdadd8828ceb102c5048bb29cba17147d84a135d
Matthieu Mota <matthieumota@gmail.com>
2023-07-20 14:17:36 +0200

    Fix

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -11,7 +11,7 @@ class Actor extends Model
     use HasFactory;
 
     protected $casts = [
-        'birthday' => 'date',
+        'birthday' => 'date', // String devient Carbon
     ];
 
     protected $guarded = [];
